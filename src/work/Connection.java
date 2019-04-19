1a4699d29dcff859ec0cf933b8bd3e6b8ee4b076
庞敏 <@DESKTOP-4O62369>
2019-04-19 18:08:43 +0800

    hello

---
+++ src/work/Connection.java
@@ -0,0 +1,23 @@
+package work;
+import java.util.*;
+public class Connection {
+	public static void main(String[] args) {
+		int count = 0;
+		Scanner factor = new Scanner(System.in);
+		String f1 = factor.nextLine();
+		HashMap<Character,Integer> map = new HashMap<>();
+		char[] f2 = f1.toCharArray();
+		for(char i : f2) {
+			if(map.containsKey(i)) {
+				//´æÔÚ¼ü
+				count = map.get(i);
+				count ++;
+				map.put(i, count);
+			}else {
+				//²»´æÔÚ¼ü
+				map.put(i,1);
+			}
+		}
+		System.out.println(map);
+	}
+}
